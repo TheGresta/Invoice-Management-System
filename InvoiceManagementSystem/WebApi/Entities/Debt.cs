a55fd1afeeba5454ed3f741a760004665b4cd2a4
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-09-04 13:05:11 +0300

    Commit 6 | Entities Debt Created

---
+++ InvoiceManagementSystem/WebApi/Entities/Debt.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Entities
+{
+  public class Debt
+  {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public int CustomerID { get; set; }
+    public int ApartmentID { get; set; }
+    public string CustomerName { get; set; }
+    public int Cost { get; set; }
+  }
+}
