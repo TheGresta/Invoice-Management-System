b1c8fa1f0808c049a1377bd710604d50fb3ef32f
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-09-07 18:26:13 +0300

    Commit 31 | Entity File Path Changed

--- InvoiceManagementSystem.Entity/Concrete/Block.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.Entity;
-
-namespace RezervationSystem.Entity.Concrete
-{
-    public class Block : BaseEntity
-    {
-        public string Name { get; set; }
-    }
-}
--- InvoiceManagementSystem.Entity/Concrete/Style.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.Entity;
-
-namespace RezervationSystem.Entity.Concrete
-{
-    public class Style : BaseEntity
-    {
-        public string Name { get; set; }
-    }
-}
